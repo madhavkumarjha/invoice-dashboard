c337ab861586b84f5e4bcc4780e9ee04f9e1846b
madhav kumar <madhavxman@gmail.com>
2025-07-24 23:15:01 +0530

    .env file added secure firebase

--- src/firebase/firebaseConfig.js
+++ src/firebase/firebaseConfig.js
@@ -8,13 +8,13 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBfdk1RMYqO-ybjx5qFnxn8RHVAm0TruT4",
-  authDomain: "to-do-list-c909f.firebaseapp.com",
-  projectId: "to-do-list-c909f",
-  storageBucket: "to-do-list-c909f.firebasestorage.app",
-  messagingSenderId: "596386384442",
-  appId: "1:596386384442:web:5ef2662f93f326dfea4303",
-  measurementId: "G-CR50ND8JFM"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
